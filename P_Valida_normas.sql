884bbddafb2aedfe7d553abdac79348fe2d162d3
Anabela <anabela.rossi@gmail.com>
2014-07-12 15:51:28 -0300

    Actualizo procedimientos y funcion valida tablas

--- P_Valida_normas.sql
+++ P_Valida_normas.sql
@@ -1,9 +1,26 @@
-CREATE PROCEDURE p_validar_normativa
+/* ====================================================== */
+/* =================  TP FINAL PUNTO 2  ================= */
+/* =                                                    = */
+/* =  DEVUELVE TODOS LOS OBJETOS DEL TIPO SOLICIDATO  	= */
+/* =  QUE NO CUMPLEN CON LA NORMATIVA				  	= */
+/* =  PARAMETRIZACIÓN:								   == */
+/* =  VALIDAR TABLAS: 'T' 							   == */
+/* =  VALIDAR PROCEDIMIENTOS: 'P'					   == */
+/* =  VALIDAR CLAVES FORANEAS:  'FK'				   == */
+/* =  VALIDAR CLAVES PRIMERIAS: 'PK'				   == */
+/* =  VALIDAR INDICES: 'I'			 				   == */
+/* =  VALIDAR TRIGERS: 'TR' 						   == */
+/* =  VALIDAR UNIQUES: 'U'							   == */
+/* =  VALIDAR CHECKS: 'C'							   == */
+/* =  VALIDAR VISTAS: 'V'							   == */
+/* ====================================================== */
 
-	@varTipoObjeto VARCHAR(50)
-	
+ALTER PROCEDURE p_validar_normativa
+
+	@varTipoObjeto VARCHAR(50)	
 AS
 BEGIN
+declare @prueba varchar(255)
 
 IF (UPPER(@varTipoObjeto) = 'T') /* TABLAS */
 	BEGIN
@@ -14,43 +31,80 @@ IF (UPPER(@varTipoObjeto) = 'T') /* TABLAS */
 								SELECT name
 								from sys.objects
 								where SUBSTRING( name, 1, 3 ) IN ('ARQ','GEN','MKT','PRT','PVN','RSV','SYA','TMP','WEB') AND
-									  SUBSTRING( name, 4, 1) = CHAR(95) ) 
+									  SUBSTRING( name, 4, 1) = CHAR(95) AND
+									  SUBSTRING(name,5,1) =  UPPER(SUBSTRING(name,5,1))
+									  )
 	END
 
 ELSE IF (UPPER(@varTipoObjeto) = 'P')  /* PROCEDIMIENTOS */
 	BEGIN
+	
 			SELECT name as 'Prodecimientos fuera de norma'
 				FROM sys.objects
 				WHERE TYPE = 'P'
 					AND name NOT IN (
 								SELECT name
 								from sys.objects
-								where LEFT( NAME, 2 ) = 'P' + CHAR(95)  AND
+								where LEFT( NAME, 2 ) = 'p' + CHAR(95)  AND
 								SUBSTRING( name, 3, 3 ) IN ('ARQ','GEN','MKT','PRT','PVN','RSV','SYA','TMP','WEB') AND
-								SUBSTRING( name, 6, 1) = CHAR(95) ) 
-
+								SUBSTRING( name, 6, 1) = CHAR(95) AND 
+								SUBSTRING( name, 7, 1) =  UPPER(SUBSTRING( name, 7, 1))
+								) 
 	END
 
-ELSE IF (UPPER(@varTipoObjeto) = 'I')				/* INDICES */				
+
+ELSE IF (UPPER(@varTipoObjeto) = 'I')												/* INDICES */				
 		BEGIN
-			SELECT name as 'Indices fuera de norma'
-				FROM sys.indexes
-				WHERE not (LEFT( name, 3 ) = 'ix'+CHAR(95))  /* QUE NO EMPIECE CON IX_ */
-				AND name not in (						     /* QUE EL NOMBRE NO SEA TENGA NOMBRE DE TALBA */       
-								SELECT name					 /* PREFIJO Y LE SIGUE UN _ */
-								from sys.objects
-								where 
-								SUBSTRING( name, 4, 3 ) IN ('ARQ','GEN','MKT','PRT','PVN','RSV','SYA','TMP','WEB') AND
-								SUBSTRING( name, 5, 1) = CHAR(95) ) 
-								
-		/*	FALTA RESOLVER COMO COMPARAR LO QUE SIGUE DE IX_ CON QUE SEA TIPO TABLA */
+			DECLARE itest cursor for
+				select t.name as ntabla, i.name as nindice 
+					from sys.indexes i
+					join sys.tables t
+					on i.object_id = t.object_id
+		
+			DECLARE @listaindices TABLE(nindice nvarchar(max))
+			DECLARE @ntabla nvarchar(max)
+			DECLARE @nindice nvarchar(max)
+			DECLARE @largo INT
+			DECLARE @mistring nvarchar(max)
+			DECLARE @nombrein varchar(max)
+			
+			open itest
+			fetch next from itest into @ntabla, @nindice
+			while @@fetch_status = 0
+				begin
+		
+				set @largo = LEN ('ix'+CHAR(95)+@ntabla)
+				
+				set @mistring = SUBSTRING (@nindice, 1, @largo)
+				
+				set @nombrein = REPLACE(@nindice, @mistring + CHAR(95), '')
+					
+					if (@mistring + CHAR(95) <> 'ix'+ CHAR(95)+ @ntabla + CHAR(95))
+					     OR (left(@nombrein,1) = lower(left(@nombrein,1))  )		
+						BEGIN
+						INSERT INTO @listaindices VALUES (@nindice)
+			
+					END
+			
+				fetch next from itest
+				into @ntabla, @nindice
+				end
+			close itest
+			deallocate itest
+			
+			select nindice as 'Indices fuera de norma' from @listaindices
+
 		END
 								
 END		
 GO				
 
-EXEC  p_validar_normativa 'I'
+EXEC  p_validar_normativa 'I' 
 
 EXEC  p_validar_normativa 'P'	
 
 EXEC  p_validar_normativa 'T'	
+
+
+
+
--- Punto1_tablas.sql
+++ Punto1_tablas.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION validarTablas (@tabla nvarchar(max), 
+ALTER FUNCTION validarTablas (@tabla nvarchar(max), 
 								@tipo nvarchar(2))
 RETURNS INT
 AS
@@ -6,6 +6,7 @@ AS
   DECLARE @ret INT
   IF (SUBSTRING(@tabla,1,3) IN ('ARQ', 'GEN', 'MKT', 'PRT', 'PVN', 'RSV', 'SYA', 'TMP', 'WEB') 
    AND SUBSTRING(@tabla,4,1) = '_') 
+    AND SUBSTRING(@tabla,5,1) =  UPPER(SUBSTRING(@tabla,5,1))
    BEGIN SET @ret = 0
    END
   ELSE
@@ -13,4 +14,4 @@ AS
   RETURN @ret
   END
   
-SELECT dbo.validarTablas('WEB_general', 'U')
+SELECT dbo.validarTablas('ARQ_Test', 'U')
