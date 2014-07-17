ec3872c9bb77510ac3e2736d298196f13851c958
Anabela <anabela.rossi@gmail.com>
2014-07-17 11:11:00 -0300

    .
    El commit anterior hizo merge, resubo

--- Punto1_tablas.sql
+++ Punto1_tablas.sql
@@ -1,17 +1,12 @@
-ALTER FUNCTION validarTablas (@tabla nvarchar(max), 
+CREATE FUNCTION validarTablas (@tabla nvarchar(max), 
 								@tipo nvarchar(2))
 RETURNS INT
 AS
   BEGIN
   DECLARE @ret INT
   IF (SUBSTRING(@tabla,1,3) IN ('ARQ', 'GEN', 'MKT', 'PRT', 'PVN', 'RSV', 'SYA', 'TMP', 'WEB') 
-<<<<<<< HEAD
    AND (SUBSTRING(@tabla,4,1) = '_') 
    AND (SUBSTRING( @tabla, 5, 1) = UPPER(SUBSTRING( @tabla, 5, 1) )	))
-=======
-   AND SUBSTRING(@tabla,4,1) = '_') 
-    AND SUBSTRING(@tabla,5,1) =  UPPER(SUBSTRING(@tabla,5,1))
->>>>>>> origin/master
    BEGIN SET @ret = 0
    END
   ELSE
@@ -19,8 +14,8 @@ AS
   RETURN @ret
   END
   
-<<<<<<< HEAD
+
 SELECT dbo.validarTablas('MKT_Tabla3', 'U')
-=======
-SELECT dbo.validarTablas('ARQ_Test', 'U')
->>>>>>> origin/master
+
+SELECT dbo.validarTablas('ARQ_test', 'U')
+
